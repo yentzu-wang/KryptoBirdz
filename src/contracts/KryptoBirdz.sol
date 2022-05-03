d50767a12e16817159877a8c3a45f947e3141a02
Frank Wang <frank7300228@gmail.com>
2022-05-04 02:46:22 +0800

    Finish 120

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -4,5 +4,14 @@ pragma solidity >=0.8.0;
 import "./ERC721Connector.sol";
 
 contract KryptoBirdz is ERC721Connector {
+    string[] public kryptoBirdz;
+
     constructor() ERC721Connector("KryptoBirdz", "KBIRDS") {}
+
+    function mint(string calldata _kryptoBird) public {
+        kryptoBirdz.push(_kryptoBird);
+        uint256 _id = kryptoBirdz.length - 1;
+
+        _mint(msg.sender, _id);
+    }
 }
