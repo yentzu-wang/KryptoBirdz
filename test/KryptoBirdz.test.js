7e39c70f01dfcfd2d9006d125b3573371985f013
Frank Wang <frank7300228@gmail.com>
2022-05-05 04:07:51 +0800

    Finish 178

--- test/KryptoBirdz.test.js
+++ test/KryptoBirdz.test.js
@@ -7,9 +7,12 @@ require("chai").use(require("chai-as-promised")).should()
 contract("KryptoBirdz", async (accounts) => {
   let contract
 
+  before(async () => {
+    contract = await KryptoBirdz.deployed()
+  })
+
   describe("deployment", async () => {
     it("contract is deployed", async () => {
-      contract = await KryptoBirdz.deployed()
       const address = contract.address
 
       assert.notEqual(address, "")
@@ -26,6 +29,24 @@ contract("KryptoBirdz", async (accounts) => {
     it("has a symbol", async () => {
       const symbol = await contract.symbol()
       assert.equal(symbol, "KBIRDZ")
+    })
+  })
+
+  describe("minting", async () => {
+    it("creates a new token", async () => {
+      const result = await contract.mint("https...1")
+      const totalSupply = await contract.totalSupply()
+
+      assert.equal(totalSupply, 1)
+      const event = result.logs[0].args
+      assert.equal(
+        event._from,
+        "0x0000000000000000000000000000000000000000",
+        "from the contract"
+      )
+      assert.equal(event._to, accounts[0], "to is msg.sender")
+
+      await contract.mint("https...1").should.be.rejected
     })
   })
 })
