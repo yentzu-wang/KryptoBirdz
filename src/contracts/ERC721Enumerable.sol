4041cbfc3f15d8bba2b3aced45e6d9ea51a63fd2
Frank Wang <frank7300228@gmail.com>
2022-05-05 02:31:33 +0800

    Finish 166

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -2,8 +2,9 @@
 pragma solidity >=0.8.0;
 
 import "./ERC721.sol";
+import "./interfaces/IERC721Enumerable.sol";
 
-contract ERC721Enumerable is ERC721 {
+contract ERC721Enumerable is ERC721, IERC721Enumerable {
     uint256[] private _allTokens;
     mapping(uint256 => uint256) private _allTokensIndex;
     mapping(address => uint256[]) private _ownedTokens;
