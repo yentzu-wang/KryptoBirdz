20f570affb5a85df5ff7037ba1fb454b92191637
Frank Wang <frank7300228@gmail.com>
2022-05-04 13:40:26 +0800

    Finish 141

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -5,18 +5,41 @@ import "./ERC721.sol";
 
 contract ERC721Enumerable is ERC721 {
     uint256[] private _allTokens;
-    mapping(uint256 => uint256) private _alltokensIndex;
+    mapping(uint256 => uint256) private _allTokensIndex;
     mapping(address => uint256[]) private _ownedTokens;
     mapping(uint256 => uint256) private _ownedTokensIndex;
 
     function _mint(address to, uint256 tokenId) internal override(ERC721) {
         super._mint(to, tokenId);
 
-        _addTokensToToTotalSupply(tokenId);
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokensToOwnerEnumeration(to, tokenId);
     }
 
-    function _addTokensToToTotalSupply(uint256 tokenId) private {
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
+    }
+
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
+
+    function tokenByIndex(uint256 index) public view returns (uint256) {
+        require(index < totalSupply(), "Index out of range");
+
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        public
+        view
+        returns (uint256)
+    {
+        require(index < balanceOf(owner), "Index out of range");
+
+        return _ownedTokens[owner][index];
     }
 
     function totalSupply() public view returns (uint256) {
