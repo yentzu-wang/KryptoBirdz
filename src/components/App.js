3a2f1ab5bb50a04f28ab71ee43c85580951eb40e
Frank Wang <frank7300228@gmail.com>
2022-05-06 22:56:35 +0800

    Finish 236

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-import React, { useEffect, useReducer } from "react"
+import React, { useEffect, useReducer, useRef } from "react"
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit"
 import Web3 from "web3"
 import detectEthereumProvider from "@metamask/detect-provider"
 import KryptoBirdz from "../abis/KryptoBirdz.json"
 
+import "./App.css"
+
 const App = () => {
+  const inputRef = useRef()
   const [state, setState] = useReducer(
     (state, newState) => ({ ...state, ...newState }),
     {
@@ -50,10 +61,12 @@ const App = () => {
       const totalSupply = await contract.methods.totalSupply().call()
 
       setState({ totalSupply })
+      const kBirds = []
       for (let i = 1; i <= totalSupply; i++) {
         const kryptoBird = await contract.methods.kryptoBirdz(i - 1).call()
-        setState({ kryptoBirdz: [...state.kryptoBirdz, kryptoBird] })
+        kBirds.push(kryptoBird)
       }
+      setState({ kryptoBirdz: kBirds })
     } else {
       window.alert("Smart contract not deployed")
     }
@@ -69,7 +82,7 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div className="container-filled">
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 sahdow">
         <div
           className="navbar-brand col-sm-3 col-md-3 mr-0"
@@ -87,7 +100,55 @@ const App = () => {
         <div className="row">
           <main role="main" className="col-lg-12 d-flex text-center">
             <div className="content mr-auto ml-auto" style={{ opacity: "0.8" }}>
-              <h1 style={{ color: "white" }}>KryptoBirdz - NFT Marketplace</h1>
+              <h1 style={{ color: "black" }}>KryptoBirdz - NFT Marketplace</h1>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault()
+
+                  const kryptoBird = inputRef.current.value
+                  mint(kryptoBird)
+                }}
+              >
+                <input
+                  type="text"
+                  placeholder="Add a file location"
+                  className="form-control mb-1"
+                  ref={inputRef}
+                />
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-black"
+                  value="MINT"
+                />
+              </form>
+              <hr />
+              <div className="row textCenter">
+                {state.kryptoBirdz.map((kryptoBird, index) => (
+                  <div key={kryptoBird}>
+                    <div>
+                      <MDBCard
+                        className="token img"
+                        style={{ maxWidth: "22rem" }}
+                      >
+                        <MDBCardImage
+                          src={kryptoBird}
+                          position="top"
+                          style={{ marginRight: 4 }}
+                          height="250rem"
+                        />
+                        <MDBCardBody>
+                          <MDBCardTitle>KryptoBirdz</MDBCardTitle>
+                          <MDBCardText>
+                            The KryptoBirdz are 20 uniquely generated KBirdz
+                            from metaverse
+                          </MDBCardText>
+                          <MDBBtn href={kryptoBird}>Download</MDBBtn>
+                        </MDBCardBody>
+                      </MDBCard>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </main>
         </div>
